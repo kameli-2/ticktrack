53e71b1a8781cc893515b2c18b2850ccc8da0124
Zsolt Szilagyi <zsolt.szilagyi@hiq.fi>
2024-12-17 08:18:16 +0200

    feat: tab style navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,16 @@
 import { Link, useLocation } from "react-router-dom";
 import styles from "./Navigation.module.css";
+import { ReactNode } from "react";
+
+type Page = {
+  path: string,
+  name: string,
+  selected?: boolean,
+}
 
 export default function Navigation() {
   const location = useLocation();
   const selected = location.pathname;
-
-  type Page = {
-    path: string,
-    name: string,
-    selected?: boolean,
-  }
 
   const pages: Page[] = [
     {
@@ -23,12 +24,24 @@ export default function Navigation() {
   ];
 
   pages.forEach(page => page.selected = page.path === selected);
-  const currentPage = pages.find(page => page.selected);
 
   return <header className="App-header">
-    <h1>{currentPage?.name || 'Time Tracker'}</h1>
-    <nav className={styles.navigation}>
-      {pages.filter(page => !page.selected).map(page => <Link key={page.name} to={page.path}>{page.name}</Link>)}
-    </nav>
+    <h1>TickTrack</h1>
+    <Tabs>
+      {pages.map(page => <Tab selected={page.selected} path={page.path} name={page.name} />)}
+    </Tabs>
   </header>
+}
+
+function Tabs(props: { children?: ReactNode }) {
+  return <nav className={styles.tabs}>
+    {props.children}
+  </nav>
+}
+
+function Tab(props: Page) {
+  const classes = [styles.tab];
+  if (props.selected) classes.push(styles['tab--selected']);
+
+  return <Link className={classes.join(' ')} key={props.name} to={props.path}>{props.name}</Link>
 }
